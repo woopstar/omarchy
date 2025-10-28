0e5c22ac380660ad682e25204f12fbd5a3054863
Ryan Hughes <ryan@heyoodle.com>
2025-10-28 02:33:06 -0400

    Add the keyring for manual installs

--- install/preflight/pacman.sh
+++ install/preflight/pacman.sh
@@ -6,6 +6,13 @@ if [[ -n ${OMARCHY_ONLINE_INSTALL:-} ]]; then
   sudo cp -f ~/.local/share/omarchy/default/pacman/pacman.conf /etc/pacman.conf
   sudo cp -f ~/.local/share/omarchy/default/pacman/mirrorlist /etc/pacman.d/mirrorlist
 
+  sudo pacman-key --recv-keys 40DFB630FF42BCFFB047046CF0134EE680CAC571 --keyserver keys.openpgp.org
+  sudo pacman-key --lsign-key 40DFB630FF42BCFFB047046CF0134EE680CAC571
+
+  sudo pacman -Sy
+  sudo pacman -S --noconfirm --needed omarchy-keyring
+
+
   # Refresh all repos
   sudo pacman -Syu --noconfirm
 fi
