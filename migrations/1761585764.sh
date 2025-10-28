c17e4f57ffe0839e02b593da7a7ad646a72d37ba
David Heinemeier Hansson <david@hey.com>
2025-10-28 08:59:37 +0100

    No need for this either

--- migrations/1761585764.sh
+++ migrations/1761585764.sh
@@ -1,10 +1,3 @@
 echo "Disable Nvim news alerts box"
 
-cat <<EOF
-
-This will add a config file to disable it:
-  ~/.config/nvim/lua/plugins/disable-news-alert.lua
-
-EOF
-
 cp /usr/share/omarchy-nvim/config/lua/plugins/disable-news-alert.lua ~/.config/nvim/lua/plugins/disable-news-alert.lua
