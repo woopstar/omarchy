ae10133b5edb0521d7551707ac10c3a34461c5cc
Ryan Hughes <ryan@heyoodle.com>
2025-11-02 17:57:25 -0500

    Fix neovim launching from files
    Because of [hardcoded terminals in
    glib](https://gitlab.gnome.org/GNOME/glib/-/blob/main/gio/gdesktopappinfo.c#L2755)
    the previous fix still doesn't work well with Alacritty and Kitty.
    
    Using our `omarchy-launch-terminal` aliased to `xdg-terminal-exec` does
    the trick and doesn't introduce any new dependencies or rely on a lib
    that isn't stable.

---
+++ install/config/xdg-terminal-exec.sh
@@ -0,0 +1,3 @@
+# Solve for hardcoded glib terminals
+# https://github.com/basecamp/omarchy/issues/1852
+sudo ln -s $OMARCHY_PATH/bin/omarchy-launch-terminal /usr/local/bin/xdg-terminal-exec
---
+++ migrations/1762121827.sh
@@ -0,0 +1,4 @@
+echo "Setting up xdg-terminal-exec for gtk-launch terminal support"
+# Solve for hardcoded glib terminals
+# https://github.com/basecamp/omarchy/issues/1852
+sudo ln -s $OMARCHY_PATH/bin/omarchy-launch-terminal /usr/local/bin/xdg-terminal-exec
