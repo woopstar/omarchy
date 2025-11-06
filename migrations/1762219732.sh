5fb29c199a034b50b6a72519b3b8c2801dd6466a
Ryan Hughes <ryan@heyoodle.com>
2025-11-06 11:30:02 -0500

    Revert "Update screensaver with 2x version"
    This reverts commit b1b6e5d2a7428f3a3ea7a96b6e51548e53cb79b7.

--- migrations/1762219732.sh
+++
@@ -1,4 +0,0 @@
-echo "Update Omarchy screensaver txt (if unchanged)"
-if cmp -s $OMARCHY_PATH/logo.txt ~/.config/omarchy/branding/screensaver.txt; then
-  cp "$OMARCHY_PATH/config/omarchy/branding/screensaver.txt" ~/.config/omarchy/branding/screensaver.txt
-fi
