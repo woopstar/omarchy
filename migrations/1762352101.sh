870356ecc3c66869a333c2cb00c5e663962bf11d
David Heinemeier Hansson <david@hey.com>
2025-11-06 08:32:07 +0100

    Fix max_results to live under [providers]
    Re #3175

--- migrations/1762352101.sh
+++ migrations/1762352101.sh
@@ -1,5 +1,5 @@
 echo "Increase Walker limit on how many entries can be shown to 256"
 
 if ! grep -q "max_results" ~/.config/walker/config.toml; then
-  sed -i "1i max_results = 256           # 256 should be enough for everyone" ~/.config/walker/config.toml
+  sed -i '/^\[providers\]$/a max_results = 256' ~/.config/walker/config.toml
 fi
