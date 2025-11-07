6cdf4a3790ac657c0328bcddda796fc4f1eee7a4
Ryan Hughes <ryan@heyoodle.com>
2025-11-07 14:46:29 -0500

    Remove any alternative limine.conf files

---
+++ migrations/1762446739.sh
@@ -0,0 +1,10 @@
+echo "Remove alternative limine.conf files"
+
+if [ ! -f /boot/limine.conf ]; then
+  echo "Error: /boot/limine.conf does not exist. Do not reboot without resolving this issue!"
+  exit 1
+fi
+
+[ -f /boot/EFI/limine/limine.conf ] && sudo rm -f /boot/EFI/limine/limine.conf
+[ -f /boot/EFI/BOOT/limine.conf ] && sudo rm -f /boot/EFI/BOOT/limine.conf
+[ -f /boot/limine/limine.conf ] && sudo rm -f /boot/limine/limine.conf
