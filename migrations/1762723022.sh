cbccc456a0f579b1715b1e8daa86f13af0d760e4
Andreas Nigg <andreas.nigg@devopsandmore.com>
2025-11-13 11:12:54 +0100

    fix migration for configs where no tray config is given

--- migrations/1762723022.sh
+++ migrations/1762723022.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# This migration script adds the custom/omarchy-bluetooth-battery-status module to Waybar configuration.
+# This migration script adds the custom peripherals battery monitoring module to Waybar configuration.
 # It creates a backup of the existing configuration before making changes.
 # It checks for the existence of the module and its configuration to avoid duplicates.
-# This should preserve custom wayber configurations - which are possibly plentiful.
+# This should preserve custom waybar configurations - which are possibly plentiful.
 
 CONFIG_FILE="$HOME/.config/waybar/config.jsonc"
 BACKUP_FILE="$HOME/.config/waybar/config.jsonc.backup.$(date +%s)"
@@ -40,39 +40,30 @@ awk '
     { print }
 ' "$CONFIG_FILE" >"$CONFIG_FILE.tmp"
 mv "$CONFIG_FILE.tmp" "$CONFIG_FILE"
-
 echo "Added to modules-right array"
 
-# Step 2: Add module configuration after the tray config (last module config)
+# Step 2: Add module configuration right after modules-right array closes
 awk '
-    BEGIN { in_tray = 0; brace_count = 0 }
+    BEGIN { in_modules_right = 0 }
 
-    # Detect start of tray configuration
-    /"tray":[[:space:]]*{/ {
-        in_tray = 1
-        brace_count = 1
+    # Detect modules-right array
+    /"modules-right":[[:space:]]*\[/ {
+        in_modules_right = 1
         print
         next
     }
 
-    # Track braces when in tray config
-    in_tray == 1 {
-        if (/{/) brace_count++
-        if (/}/) brace_count--
-
+    # When we find the closing bracket of modules-right
+    in_modules_right == 1 && /^[[:space:]]*\],?[[:space:]]*$/ {
         print
-
-        # When we close the tray config
-        if (brace_count == 0) {
-            in_tray = 0
-            print "  \"custom/devices-battery\": {"
-            print "    \"exec\": \"omarchy-peripherals-battery-status\","
-            print "    \"return-type\": \"json\","
-            print "    \"interval\": 30,"
-            print "    \"format\": \"{}\","
-            print "    \"tooltip\": true"
-            print "  },"
-        }
+        print "  \"custom/devices-battery\": {"
+        print "    \"exec\": \"omarchy-peripherals-battery-status\","
+        print "    \"return-type\": \"json\","
+        print "    \"interval\": 30,"
+        print "    \"format\": \"{}\","
+        print "    \"tooltip\": true"
+        print "  },"
+        in_modules_right = 0
         next
     }
 
@@ -80,4 +71,7 @@ awk '
 ' "$CONFIG_FILE" >"$CONFIG_FILE.tmp"
 mv "$CONFIG_FILE.tmp" "$CONFIG_FILE"
 
+echo "Added module configuration"
+echo ""
+echo "Migration complete!"
 omarchy-refresh-waybar
