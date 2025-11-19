c50003a9962d87356c08d641a5aaad4fc8020fa6
David Heinemeier Hansson <david@hey.com>
2025-11-19 11:36:15 +0100

    Make sure to stop and start in the new fashion

--- migrations/1762873545.sh
+++ migrations/1762873545.sh
@@ -2,5 +2,7 @@ pkill elephant
 elephant service enable
 systemctl --user start elephant.service
 
+pkill walker
 mkdir -p ~/.config/autostart/
 cp $OMARCHY_PATH/autostart/walker.desktop ~/.config/autostart/
+systemctl --user start app-walker@autostart.service
