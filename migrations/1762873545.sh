2c1a761b88c372e0e07bb7c0a2f85edd32226d8d
David Heinemeier Hansson <david@hey.com>
2025-11-19 11:42:01 +0100

    Walker is not run as a systemd service

--- migrations/1762873545.sh
+++ migrations/1762873545.sh
@@ -5,4 +5,4 @@ systemctl --user start elephant.service
 pkill walker
 mkdir -p ~/.config/autostart/
 cp $OMARCHY_PATH/autostart/walker.desktop ~/.config/autostart/
-systemctl --user start app-walker@autostart.service
+setsid walker --gapplication-service &
