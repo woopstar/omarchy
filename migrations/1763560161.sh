4c8ee6cc1a3954e7c7583f37f67533cee5e222e9
David Heinemeier Hansson <david@hey.com>
2025-11-20 14:21:29 +0100

    There could have been another format-no-controller actually

--- migrations/1763560161.sh
+++ migrations/1763560161.sh
@@ -1,6 +1,4 @@
 echo "Hide Bluetooth module in waybar if there's no BT controller"
 
-if ! grep -q "format-no-controller" ~/.config/waybar/config.jsonc; then
-  sed -i '/format-connected": "",/a\    "format-no-controller": "",' ~/.config/waybar/config.jsonc
-  omarchy-restart-waybar
-fi
+sed -i '/format-connected": "",/a\    "format-no-controller": "",' ~/.config/waybar/config.jsonc
+omarchy-restart-waybar
